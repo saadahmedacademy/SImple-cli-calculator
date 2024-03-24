6c4cffca96b51b65c49bbaa177e74e9f29bad2b9
Muhammad Saad Ahmed <147624130+saadahmedacademy@users.noreply.github.com>
2024-03-24 23:19:39 +1200

    This is a simple cli base calculator

---
+++ SImple-cli-calculator/index.js
@@ -0,0 +1,36 @@
+#! /use/bin/node
+import inquirer from 'inquirer';
+let answers = await inquirer.prompt([
+    {
+        message: "Enter Your Fisst Number : ",
+        type: "number",
+        name: 'input1'
+    },
+    {
+        message: "Enter Your Second Number : ",
+        type: "number",
+        name: 'input2'
+    },
+    {
+        message: "Enter Your Fisst Number : ",
+        type: "list",
+        name: 'operation',
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+    }
+]);
+if (answers.operation === "Addition") {
+    console.log(answers.input1 + answers.input2);
+}
+else if (answers.operation === "Subtraction") {
+    console.log(answers.input1 - answers.input2);
+}
+else if (answers.operation === "Multiplication") {
+    console.log(answers.input1 * answers.input2);
+}
+else if (answers.operation === "Division") {
+    console.log(answers.input1 / answers.input2);
+}
+else {
+    console.log("Please Select Valid Operator");
+}
+console.log(answers);
